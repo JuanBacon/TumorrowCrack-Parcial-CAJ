69bc0e7734d8c7a951467ae4e6a11602e8ff8c3d
Juan Diego Bustamante <Juan.Bacon.B@outlook.com>
2022-04-13 03:48:12 -0500

    login v2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
+// Declaracion de variables para el formulario de inicio de sesion
+const loginFormElement = document.getElementById('loginForm');
 const inputEmail = document.getElementById('email');
 const inputPass = document.getElementById('pass');
-const formElement = document.getElementById('loginForm');
 const inputLogin = document.getElementById('login');
-let word = ""
 let key = 10;
 let encrypt = true;
+let dencrypt = false;
 // localStorage.setItem("user","juan_die.bustamante@uao.edu.co")
-// localStorage.setItem("password", encryptPassword("unaContraseñaMuyLarga",key,true))
+// localStorage.setItem("password", encryptPassword("cualquierContraseña1234!#",key,true))
 
 // TODO: Clase artista: nombre, lista de spotify, imagen, perfil'Descripcion'
 // Clase Imagen: imagen, titulo, alt'texto alternativo'
@@ -18,7 +19,6 @@ class Artist {
         this.description = description;
     }
 }
-
 class Img {
     constructor(img, tittle, alt){
         this.img = img;
@@ -26,68 +26,68 @@ class Img {
         this.alt = alt;
     }
 }
-
-
-
-
-formElement.addEventListener('submit', (event) => {
-    event.preventDefault();
-    pass = inputPass.value;
-    let mypassword = encryptPassword(pass, key, true);
-    let myuser = inputEmail.value;
-    console.log("Logging in with user:",myuser, "and password:", mypassword);
-    let localpassword = localStorage.getItem('password');
-    let localuser = localStorage.getItem('user');
-    //validar contraseña
-
-    if((mypassword === localpassword) && (myuser === localuser)){
-        alert("Correct log in");
-        formElement.reset()
-        formElement.action = "../html/panel.html"
-        formElement.submit();
-    } else{
-        alert("Incorrect data")
-        formElement.reset()
-    }
-});
-
-// console.log("Encriptandooo");
-// let mypassword = encryptPassword(word, key, true);
-// console.log("Login with password:", mypassword);
-
-// localStorage.setItem('contraseña', mypassword);
-// console.log("Desencriptandoooo");
-// let passwordCracked = encryptPassword(mypassword,key,false);
-// console.log("La contraseña usada fue:", passwordCracked);
-
-
-function encryptPassword(word, key, encrypt){
-    let unicodeWord = [];
-    let newWord = ""
-    for(let i = 0; i < word.length; i++){
-        unicodeWord[i] = word.charCodeAt(i);
-    }
-    if(encrypt){
+// Comprobando que existe el elemento formulario del login
+if(loginFormElement){
+    // evento llamado al llamar el submit del formulario
+    loginFormElement.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // validacion y comparacion de los datos de inicio de sesion
+        pass = inputPass.value;
+        let mypassword = encryptPassword(pass, key, true);
+        let myuser = inputEmail.value;
+        console.log("Logging in with user:",myuser, "and password:", mypassword);
+        let localpassword = localStorage.getItem('password');
+        let localuser = localStorage.getItem('user');
+        //validar contraseña y usuario
+        if((mypassword === localpassword) && (myuser === localuser)){
+            // inicio de sesion correcto 
+            alert("Correct log in");
+            loginFormElement.reset()
+            loginFormElement.action = "../html/panel.html"
+            loginFormElement.submit();
+        } else{
+            // inicio de sesion incorrecto
+            alert("Incorrect data")
+            loginFormElement.reset()
+        }
+    });
+    // funcion que encripta o desencripta, recibe la cadena a decifrar, la lleva del cifrado y la orden de encriptado o desencriptado
+    function encryptPassword(word, key, encrypt){
+        // variables para guardar el cifrado y la cadena a retornar
+        let unicodeWord = [];
+        let newWord = ""
+        // lee y guarda en un arreglo cada uno de los caracteres en forma de codigo unicode
         for(let i = 0; i < word.length; i++){
-            unicodeWord[i] = unicodeWord[i] + key;
-            if(unicodeWord[i] > 126){
-                unicodeWord[i] = 33;
+            unicodeWord[i] = word.charCodeAt(i);
+        }
+        // depende de la orden encripta o desencripta
+        if(encrypt){
+            // encripta
+            for(let i = 0; i < word.length; i++){
+                if(unicodeWord[i] == 241 || unicodeWord[i] == 209){
+                    unicodeWord[i] = unicodeWord[i];
+                }else if((unicodeWord[i] + key) > 126){
+                    unicodeWord[i] = 33 + (key -(126 - unicodeWord[i] + 1));
+                }else {
+                    unicodeWord[i] = unicodeWord[i] + key;
+                }
+            }
+        }else{
+            // desencripta
+            for(let i = 0; i < word.length; i++){
+                if(unicodeWord[i] == 241 || unicodeWord[i] == 209){
+                    unicodeWord[i] = unicodeWord[i];
+                }else if((unicodeWord[i] - key) < 33){
+                    unicodeWord[i] = 126 - (key -(unicodeWord[i] - 33 + 1));
+                }else {
+                    unicodeWord[i] = unicodeWord[i] - key;
+                }
             }
         }
-    }else{
+        // transforma los codigos unicode en una cadena string para devolverla
         for(let i = 0; i < word.length; i++){
-            unicodeWord[i] = unicodeWord[i] - key;
-            if(unicodeWord[i] < 33){
-                unicodeWord[i] = 126;
-            }
+            newWord += String.fromCharCode(unicodeWord[i]);
         }
+        return newWord
     }
-    for(let i = 0; i < word.length; i++){
-        newWord += String.fromCharCode(unicodeWord[i]);
-    }
-    return newWord
 }
-
-
-
-
